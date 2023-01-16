9f0af178f4259b41f6b5c457bde963c857d77a08
Volodymyr Husak <volodyaog2907@gmail.com>
2023-01-16 18:06:58 +0200

    correct contactsPath

--- models/contacts.js
+++ models/contacts.js
@@ -2,7 +2,8 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-const contactsPath = path.resolve("./models/contacts.json");
+// const contactsPath = path.resolve("./models/contacts.json");
+const contactsPath = path.join(__dirname, "..", "models", "contacts.json");
 
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath, "utf8");
