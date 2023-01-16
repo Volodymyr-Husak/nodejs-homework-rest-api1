3f697302ced7778a939fb9b0320c11908d295d84
Volodymyr Husak <volodyaog2907@gmail.com>
2023-01-16 18:03:16 +0200

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,17 +1,44 @@
 const Joi = require("joi");
 
-const contactsPostSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
-const contactsPutSchema = Joi.object({
-  name: Joi.string().min(3).max(30),
-  email: Joi.string(),
-  phone: Joi.string(),
-})
-  .required()
-  .min(1);
+const contactsPostSchema = (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "missing required name field",
+    });
+  }
+  next();
+};
+
+const contactsPutSchema = (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30),
+    email: Joi.string(),
+    phone: Joi.string(),
+  })
+    .required()
+    .min(1);
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "missing fields",
+    });
+  }
+  next();
+};
 
 module.exports = {
   contactsPostSchema,
