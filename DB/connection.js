2014bd22f1fb6dc0c613d6ec9ba72712ad2d5c5b
Volodymyr Husak <volodyaog2907@gmail.com>
2023-01-17 22:56:52 +0200

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,30 +1,22 @@
 const mongoose = require("mongoose");
 
-// mongoose.set("strictQuery", false);
-// mongoose.connect(process.env.MONGO_URL, () => {
-//   console.log("Connected to MongoDB");
-// });
-
 const connectMongo = async () => {
-  //   mongoose.connect(
-  //     // process.env.MONGO_URL,
-  //     MONGO_URL,
-  //     {
-  //       userNewUrlParser: true,
-  //       useUnifiedTopology: true,
-  //     }
-  //   );
   mongoose.set("strictQuery", false);
-  mongoose.connect(
-    process.env.MONGO_URL,
-    () => {
-      console.log("Database connection successful");
-    },
-    {
-      userNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
+  mongoose
+    .connect(
+      process.env.MONGO_URL,
+      () => {
+        console.log("Database connection successful");
+      },
+      {
+        userNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
+    .catch((error) => {
+      console.warn(error);
+      process.exit(1);
+    });
 };
 
 module.exports = {
